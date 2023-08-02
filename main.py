c68e88e790d4be5e732b2820ec8eb3e31b00a546
ghw0223 <ghw2448@gmail.com>
2023-08-03 04:24:05 +0900

    함수

--- main.py
+++ main.py
@@ -16,11 +16,35 @@ import playsound
 중간맛 = ["카레","핫도그","햄버거","라면","닭도리탕","김치찌개","제육볶음"]
 순한맛 = ["볶음밥","돼지국밥","콩나물국밥","장어덮밥","스테이크덮밥","빠네스프","샌드위치","페스츄리","칼국수","짜장면","스파게티","까르보나라","돈까스","규카츠","치킨","탕수육","멘보샤","닭껍질튀김","갈비찜","된장찌개","소고기구이","돼지고기구이","닭고기구이","함박스테이크","육회","오징어튀김","새우튀김","튀김만두","오뎅탕","닭똥집","불고기"]
 
-tts = gTTS(text="원하시는 메뉴의 카테고리를 선택해 주세요.", lang='ko')
-tts.save("1.mp3")
-playsound.playsound("1.mp3")
+def TTS(contents, file_name):
 
-a,b = input("원하시는 메뉴의 카테고리를 선택해 주세요.\n").split(' ')
+    print(contents)
+    tts = gTTS(text=contents, lang='ko')
+    tts.save(file_name + ".mp3")
+    playsound.playsound(file_name + ".mp3")
+
+def wrong_input():
+
+    text = "잘못된 입력입니다."
+    TTS(text, "Wrong input")
+    exit()
+
+def no_menu():
+
+    text = "선택하신 조건과 일치하는 메뉴가 없습니다."
+    TTS(text, "no_menu"); exit()
+
+def recommend_menu(menu_list):
+
+    global text
+    text = "추천하는 메뉴는"
+    for i in menu_list : text += (' ' + i)
+    text += "입니다."
+
+text = "원하시는 메뉴의 카테고리를 선택해 주세요."
+TTS(text, "category")
+
+a,b = input().split(' ')
 
 if a =='밥': a = 밥
 elif a =='빵': a = 빵
@@ -31,7 +55,7 @@ elif a =='튀김': a = 튀김
 elif a =='국물': a = 국물
 elif a =='구이': a = 구이
 elif a =='비빔': a = 비빔
-else: print("잘못된 입력입니다."); exit()
+else: wrong_input()
 
 if b =='밥': b = 밥
 elif b =='빵': b = 빵
@@ -42,45 +66,27 @@ elif b =='튀김': b = 튀김
 elif b =='국물': b = 국물
 elif b =='구이': b = 구이
 elif b =='비빔': b = 비빔
-else: print("잘못된 입력입니다."); exit()
+else: wrong_input()
 
-text = "추천하는 메뉴는"
-recommend_menu = set(a) & set(b)
+recommend_menu0 = set(a) & set(b)
 
-for i in recommend_menu :
-    text += i
+if recommend_menu0 != set(): 
+    recommend_menu(recommend_menu0)
+    TTS(text, "recommend_menu0")
+else: no_menu()
 
-text += "입니다."
-if recommend_menu != set():
-    print("추천하는 메뉴는",recommend_menu,"입니다")
-else: print("선택하신 카테고리에 일치하는 메뉴가 없습니다."); exit()
+text = "맵기정도를 선택해 주세요."; TTS(text, "spicy")
 
-tts = gTTS(text=text, lang='ko')
-tts.save("2.mp3")
-playsound.playsound("2.mp3")
-
-text = "맵기정도를 선택해 주세요."
-
-tts = gTTS(text=text, lang='ko')
-tts.save("3.mp3")
-playsound.playsound("3.mp3")
-
-spicy = input("맵기정도를 선택해 주세요.\n")
+spicy = input()
 
 if spicy == '매운맛': spicy = 매운맛
 elif spicy == '중간맛': spicy = 중간맛
 elif spicy == '순한맛': spicy = 순한맛
-else: print("잘못된 입력입니다."); exit()
+else: wrong_input()
 
-recommend_menu1 = set(recommend_menu) & set(spicy)
+recommend_menu1 = set(recommend_menu0) & set(spicy)
 
-text = "추천하는 매뉴는"
-for i in recommend_menu1:
-    text += i
-text += "입니다."
-
-print("추천하는 메뉴는",recommend_menu1,"입니다.")
-
-tts = gTTS(text=text, lang='ko')
-tts.save("4.mp3")
-playsound.playsound("4.mp3")
+if recommend_menu1 != set():
+    recommend_menu(recommend_menu1)
+    TTS(text, "recommend_menu1")
+else: no_menu()
